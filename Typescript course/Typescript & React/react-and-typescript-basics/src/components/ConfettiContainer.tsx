1fbe88ae31669a3fb82d5b382bd55f0d4cde83d3
andjsavin <andj.savin@gmail.com>
2025-12-04 17:26:46 +0200

    Fully standartized typescript react hangman game

--- Typescript course/Typescript & React/react-and-typescript-basics/src/components/ConfettiContainer.tsx
+++ Typescript course/Typescript & React/react-and-typescript-basics/src/components/ConfettiContainer.tsx
@@ -1,6 +1,8 @@
 import Confetti from "react-confetti"
+import type {JSX} from 'react'
 
-export default function ConfettiContainer({ isGameWon }) {
+
+export default function ConfettiContainer({ isGameWon }:{isGameWon:boolean}):JSX.Element | null {
     if (!isGameWon) {
         return null
     }
